4bb1f165d138dee3f1c670b1e1bc8ac52d9993b3
QATester <test@gmail.com>
2025-07-01 17:26:01 +0530

    Updated timeout

--- src/test/java/com/orangehrm/utils/DriverFactory.java
+++ src/test/java/com/orangehrm/utils/DriverFactory.java
@@ -1,5 +1,7 @@
 package com.orangehrm.utils;
 
+import java.time.Duration;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import io.github.bonigarcia.wdm.WebDriverManager;
@@ -15,6 +17,7 @@ public class DriverFactory {
             driver = new ChromeDriver();
             driver.manage().window().maximize();
             System.out.println("Browser maximized");
+            driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(120));
         }
         return driver;
     }
